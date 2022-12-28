15ea335d269e9a355f5a96a229db07925584d30f
Mauricio Tanguila <mtanguila1@gmail.com>
2022-12-28 16:20:27 -0500

    primer commit

---
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-numeros',
+  templateUrl: './numeros.component.html',
+  styles: [
+  ]
+})
+export class NumerosComponent {
+
+  ventasNetas: number = 250560.3367;
+  porcentaje: number = 0.3567;
+
+
+}
